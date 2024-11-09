aba38c49ec0bda34df053d1a5858fab91b461c35
Michał Kątnik <michal.katnik91@gmail.com>
2024-11-09 20:33:43 +0100

    added WiFi reconnect

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,30 @@
 #include "config.h"
 #include "handlers.h"
 
+void connectToWiFi() {
+  Serial.print("\nConnecting to WiFi");
+  WiFi.begin(ssid, password);
+
+  // Blink the LED while connecting to WiFi
+  while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));  // Blink LED
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Print the IP address once connected
+  Serial.println("\nWiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Initialize mDNS
+  if (!MDNS.begin(hostname)) {
+    Serial.println("Error setting up MDNS responder!");
+    return;
+  }
+  Serial.println("mDNS responder started");
+}
+
 void setup() {
   // Initialize the LED pin as an output
   pinMode(LED_BUILTIN, OUTPUT);
@@ -36,27 +60,7 @@ void setup() {
   WiFi.setHostname(hostname);
 
   // Connect to WiFi
-  Serial.print("\nConnecting to WiFi");
-  WiFi.begin(ssid, password);
-
-  // Blink the LED while connecting to WiFi
-  while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));  // Blink LED
-    delay(500);
-    Serial.print(".");
-  }
-
-  // Print the IP address once connected
-  Serial.println("\nWiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  // Initialize mDNS
-  if (!MDNS.begin(hostname)) {
-    Serial.println("Error setting up MDNS responder!");
-    return;
-  }
-  Serial.println("mDNS responder started");
+  connectToWiFi();
 
   // Define the URL endpoints and the handler functions
   server.on("/", handleRoot);
@@ -71,10 +75,14 @@ void setup() {
 
 void loop() {
   // Set LED to the value from the previous state
-  if (digitalRead(LED_BUILTIN) != ledState) {
-    digitalWrite(LED_BUILTIN, ledState);
-  }
+  digitalWrite(LED_BUILTIN, ledState);
 
   // Handle client requests
   server.handleClient();
+
+  // Reconnect to WiFi if disconnected
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi connection lost. Reconnecting...");
+    connectToWiFi();
+  }
 }
